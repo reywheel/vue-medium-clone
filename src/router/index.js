285dd23278db17bdaedd4ec247f945f22e8aa015
wheeIer <mailbox.kolesnikov@mail.ru>
2020-11-11 23:49:21 +0300

    Сверстал страницу регистрации

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
+import AppRegister from '@/views/AppRegister'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/register',
+    name: 'register',
+    component: AppRegister
   }
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
---
+++ src/views/AppRegister.vue
@@ -0,0 +1,43 @@
+<template>
+  <el-row type="flex" class="register__row">
+    <el-col :offset="8" :span="8">
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <h3 style="text-align: center">Регистрация</h3>
+        </div>
+        <el-form @submit.native.prevent="submitHandler">
+          <el-input placeholder="Имя" v-model="name" class="app-input"></el-input>
+          <el-input placeholder="Email" v-model="email" class="app-input"></el-input>
+          <el-input placeholder="Пароль" v-model="password" show-password class="app-input"></el-input>
+          <el-button type="success" native-type="submit">Зарегистрироваться</el-button>
+        </el-form>
+      </el-card>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'AppRegister',
+  data: () => ({
+    name: '',
+    email: '',
+    password: ''
+  }),
+  methods: {
+    submitHandler() {
+      console.log('submit')
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.app-input {
+  margin-bottom: 10px;
+}
+.register__row {
+  min-height: calc(100vh - 100px);
+  align-items: center;
+}
+</style>
