004bd1b09afcdee3eca6fabdef32f20a04708629
wheeIer <mailbox.kolesnikov@mail.ru>
2020-11-16 16:35:02 +0300

    Revert "Сделал компонент FeedToggler"
    This reverts commit 7d2a1cadf1f47c60714e490d3e7b2717b68603bc.

--- src/views/GlobalFeed.vue
+++ src/views/GlobalFeed.vue
@@ -1,7 +1,9 @@
 <template>
   <el-row class="feeds__wrapper" :gutter="20">
     <el-col class="feeds__feed-area" :span="18">
-      <AppFeedToggler style="margin-bottom: 20px;" />
+      <el-tabs v-model="activeFeed">
+        <el-tab-pane label="User" name="global"></el-tab-pane>
+      </el-tabs>
       <AppFeed :api-url="apiUrl"></AppFeed>
     </el-col>
     <el-col class="feeds__sidebar-area" :span="6">
@@ -13,11 +15,10 @@
 <script>
 import AppFeed from '@/components/AppFeed'
 import AppTags from '@/components/AppTags'
-import AppFeedToggler from '@/components/AppFeedToggler'
 
 export default {
   name: 'GlobalFeed',
-  components: {AppFeedToggler, AppTags, AppFeed},
+  components: {AppTags, AppFeed},
   data: () => ({
     activeFeed: 'global',
     apiUrl: 'articles'
